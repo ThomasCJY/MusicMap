d7b88cdd9ec72fb733bfd98e197e1ece8676302e
thomas <4@qq.com>
2015-04-28 16:33:36 +0800

    v2.0 Add Google Maps API and show the first Map View

--- app/src/main/java/com/example/thomas/musicmap/MainActivity.java
+++ app/src/main/java/com/example/thomas/musicmap/MainActivity.java
@@ -5,18 +5,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new performanceFragment())
-                    .commit();
-        }
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .add(R.id.container, new performanceFragment())
+//                    .commit();
+//        }
+
     }
 
 
